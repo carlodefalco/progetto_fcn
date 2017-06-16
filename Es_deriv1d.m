f8d17cf534df8200d6d151a4ecae2893c0c49331
Patrombelli <andrea.palombelli@gmail.com>
2017-06-16 10:33:11 +0200

    Update Es_deriv1d.m

--- Es_deriv1d.m
+++ Es_deriv1d.m
@@ -1,26 +1,24 @@
 uk = linspace (0, 2 * pi, 21);
 Qk = [uk; sin(uk); zeros(size (uk))];
 n = size(uk,2) ;
-dk = zeros(3,n-1);
 Dk = zeros(size(Qk)) ;
+n=n-1;
 
+for k=0:n
+    Duk = diff(uk);
+    if k<n-1
+        ak = Duk(k+1)/(Duk(k+1)+Duk(k+2));
+        qk(:,k+1) = Qk(:,k+2)-Qk(:,k+1) ;
+        qk(:,k+2) = Qk(:,k+3)-Qk(:,k+2) ;
+        dk(:,k+1) = qk(:,k+1)./Duk(k+1);
+        dk(:,k+2) = qk(:,k+2)./Duk(k+2);
+        Dk(:,k+2) = (1-ak)*dk(:,k+1) + ak*dk(:,k+2) ;
+    end
 
-for k=1:n-1
-    Duk(k) = uk(k+1)-uk(k);
-    qk(:,k) = Qk(:,k+1)-Qk(:,k) ;
-    dk(:,k) = qk(:,k)./Duk(k) ;
 end
 
-for k=1:n-2
-    ak(k) = Duk(k)/(Duk(k)+Duk(k+1)) ;
-    Dk(:,k+1) = (1-ak(k))*dk(:,k) + ak(k)*dk(:,k+1) ;
-end
-
-
-for k=1:size(Qk,1)
-    Dk(k,1)   = 2 * dk(k, 1)   - Dk(k, 2);
-    Dk(k, end) = 2 * dk(k, end) - Dk(k, end-1);
-end
+Dk(:,1)   = 2 * dk(:, 1)   - Dk(:, 2);
+Dk(:, end) = 2 * dk(:, end) - Dk(:, end-1);
 
 figure
 quiver3 (Qk(1, :), Qk(2, :), Qk(3, :), Dk(1, :), Dk(2, :), Dk(3, :))
