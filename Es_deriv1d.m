bdb70f95cb23594f4889e614497a0a495023e00d
Patrombelli <andrea.palombelli@gmail.com>
2017-06-16 11:55:39 +0200

    Update Es_deriv1d.m

--- Es_deriv1d.m
+++ Es_deriv1d.m
@@ -1,20 +1,20 @@
+clc
+clear all
+
 uk = linspace (0, 2 * pi, 21);
 Qk = [uk; sin(uk); zeros(size (uk))];
 n = size(uk,2) ;
 Dk = zeros(size(Qk)) ;
-n=n-1;
 
-for k=0:n
-    Duk = diff(uk);
-    if k<n-1
+for k=0:n-3
+        Duk(k+1) = uk(k+2)-uk(k+1);
+        Duk(k+2) = uk(k+3)-uk(k+2);
         ak = Duk(k+1)/(Duk(k+1)+Duk(k+2));
         qk(:,k+1) = Qk(:,k+2)-Qk(:,k+1) ;
         qk(:,k+2) = Qk(:,k+3)-Qk(:,k+2) ;
         dk(:,k+1) = qk(:,k+1)./Duk(k+1);
         dk(:,k+2) = qk(:,k+2)./Duk(k+2);
         Dk(:,k+2) = (1-ak)*dk(:,k+1) + ak*dk(:,k+2) ;
-    end
-
 end
 
 Dk(:,1)   = 2 * dk(:, 1)   - Dk(:, 2);
