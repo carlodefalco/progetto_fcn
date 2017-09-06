c3aeb1ce3bb19df6d8e1e0d8dd011e4d68bf68b8
Patrombelli <andrea.palombelli@gmail.com>
2017-09-06 22:56:10 +0200

    Update test97.m

--- test97.m
+++ test97.m
@@ -2,7 +2,7 @@ clear all
 clc
 close all
 
-out = A93_94_95_test_data (20, 20, 17, 25);
+out = A93_94_95_test_data (100, 100, 17, 25);
 ii=1;
 Q(:,:,1)=out{1,1,1};
 Q(:,:,2)=out{1,1,2};
@@ -10,13 +10,19 @@ Q(:,:,3)=out{1,1,3};
 [r s o]=size(Q);
 r=r-1;
 s=s-1;
-n=s; %Number of control points
-m=r;
+n=15; %Number of control points
+m=8;
 p=3;
 q=3;
 
-[U,V,P]=Globalsurfapprox(r,s,p,q,Q,n,m)
+x=Q(:,:,1);
+y=Q(:,:,2);
+z=Q(:,:,3);
+
+ [U,V,P]=GlobSurfApprox(r,s,Q,p,q,n,m)
+ figure
 knots = {U V} ;
+% cntrl = zeros [3 , n , m];
 cntrl(1,:,:)=P(:,:,1);
 cntrl(2,:,:)=P(:,:,2);
 cntrl(3,:,:)=P(:,:,3);
