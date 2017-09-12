006ec7ed60ba55214ed21ee0c80bd7cc70bc0060
Patrombelli <andrea.palombelli@gmail.com>
2017-09-12 22:57:36 +0200

    Update test95.m

--- test95.m
+++ test95.m
@@ -2,7 +2,7 @@ clc
 clear all
 close all
 
-out = A93_94_95_test_data (30 , 30 , 17, 25);
+out = A93_94_95_test_data (7 , 5 , 17, 25);
 %Put ii=1 per prima figura,ii=2 per seconda
 ii = 2 ;
 Q(:,:,1)=out{ii,1,1};
@@ -20,8 +20,14 @@ cntrl(2,:,:)=P(:,:,2);
 cntrl(3,:,:)=P(:,:,3);
 nrb = nrbmak(cntrl,knots);
 figure
-nrbplot(nrb,[20 20]);
+nrbplot(nrb,[n m]);
+% 
 % hold on
 % plot3(P(:,:,1),P(:,:,2),P(:,:,3),'x')
 % plot3(Q(:,:,1),Q(:,:,2),Q(:,:,3),'o')
+% hold off
+% 
+% hold on
+% mesh(P(:,:,1),P(:,:,2),P(:,:,3))
+% % mesh(Q(:,:,1),Q(:,:,2),Q(:,:,3))
 % hold off
