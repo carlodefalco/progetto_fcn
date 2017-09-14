915c36240ee1d96693e41f2c3f29532c000d4cd7
Carlo de Falco <carlo.defalco@polimi.it>
2017-09-14 07:35:51 +0200

    more tests

--- A93_94_95_test_data.m
+++ A93_94_95_test_data.m
@@ -8,7 +8,7 @@
 
 function out = A93_94_95_test_data (nin, min, nplot, mplot)
 
-  [uin, vin]     = ndgrid (linspace (.1, 1, nin), linspace (0, 1, min));
+  [uin, vin]     = ndgrid (linspace (.1, 1, nin), linspace (.1, 1, min));
   [uplot, vplot] = ndgrid (linspace (.1, 1, nplot), linspace (0, 1, mplot));
   [out{1, 1, 1}, out{1, 1, 2}, out{1, 1, 3}] = cone (uin, vin);
   [out{1, 2, 1}, out{1, 2, 2}, out{1, 2, 3}] = cone (uplot, vplot);
@@ -17,7 +17,22 @@ function out = A93_94_95_test_data (nin, min, nplot, mplot)
   [uplot, vplot] = ndgrid (linspace (.1, .9, nplot), linspace (-.7, .7, mplot));
   [out{2, 1, 1}, out{2, 1, 2}, out{2, 1, 3}] = ellipsoid (uin, vin);
   [out{2, 2, 1}, out{2, 2, 2}, out{2, 2, 3}] = ellipsoid (uplot, vplot);
-  
+
+  [uin, vin]     = ndgrid (linspace (.1, .9, nin), linspace (-.7, .7, min));
+  [uplot, vplot] = ndgrid (linspace (.1, .9, nplot), linspace (-.7, .7, mplot));
+  [out{3, 1, 1}, out{3, 1, 2}, out{3, 1, 3}] = zplane (uin, vin);
+  [out{3, 2, 1}, out{3, 2, 2}, out{3, 2, 3}] = zplane (uplot, vplot);
+
+  [uin, vin]     = ndgrid (linspace (.1, .9, nin), linspace (-.7, .7, min));
+  [uplot, vplot] = ndgrid (linspace (.1, .9, nplot), linspace (-.7, .7, mplot));
+  [out{4, 1, 1}, out{4, 1, 2}, out{4, 1, 3}] = yplane (uin, vin);
+  [out{4, 2, 1}, out{4, 2, 2}, out{4, 2, 3}] = yplane (uplot, vplot);
+
+  [uin, vin]     = ndgrid (linspace (.1, 1, nin), linspace (.1, 1, min));
+  [uplot, vplot] = ndgrid (linspace (.1, 1, nplot), linspace (0, 1, mplot));
+  [out{5, 1, 1}, out{5, 1, 2}, out{5, 1, 3}] = par (uin, vin);
+  [out{5, 2, 1}, out{5, 2, 2}, out{5, 2, 3}] = par (uplot, vplot);
+
 end
 
 
@@ -38,3 +53,28 @@ function [x, y, z] = ellipsoid (u, v)
   z = C * sin (pi*v/2);
 end
 
+function [x, y, z] = zplane (u, v)
+  A = 10;
+  B = 7;
+  C = 6;
+  x = A * u;
+  y = B * v;
+  z = C * ones (size (u));
+end
+
+function [x, y, z] = yplane (u, v)
+  A = 10;
+  B = 7;
+  C = 6;
+  x = A * u;
+  z = B * v;
+  y = C * ones (size (u));
+end
+
+function [x, y, z] = par (u, v)
+  R = 10;
+  H = 10;
+  z = (H*u);
+  y = (R*v);
+  x = H*u.^2;
+end
--- test95.m
+++ test95.m
@@ -2,9 +2,9 @@ clc
 clear all
 close all
 
-out = A93_94_95_test_data (7 , 5 , 17, 25);
+out = A93_94_95_test_data (5, 6, 17, 25);
 %Put ii=1 per prima figura,ii=2 per seconda
-ii = 2 ;
+ii = 3;
 Q(:,:,1)=out{ii,1,1};
 Q(:,:,2)=out{ii,1,2};
 Q(:,:,3)=out{ii,1,3};
@@ -20,7 +20,21 @@ cntrl(2,:,:)=P(:,:,2);
 cntrl(3,:,:)=P(:,:,3);
 nrb = nrbmak(cntrl,knots);
 figure
-nrbplot(nrb,[n m]);
+nrbplot(nrb,[17 25]);
+
+figure
+mesh (Q(:, :, 1), Q(:, :, 2), Q(:, :, 3), 'marker', 'x', 'edgecolor', 'r')
+hold on
+mesh (P(:, :, 1), P(:, :, 2), P(:, :, 3), 'marker', 'o', 'edgecolor', 'k')
+hold off
+
+figure
+for ii = 1 : numel (P (1, :, 1));
+  plot3 (P (ii, :, 1), P (ii, :, 2), P (ii, :, 3), 'k')
+  hold on
+end
+hold off
+
 % 
 % hold on
 % plot3(P(:,:,1),P(:,:,2),P(:,:,3),'x')
